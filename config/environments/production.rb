7c32778ebb5e1118dffca8db889d082bf47e4b83
taka292 <66032836+taka292@users.noreply.github.com>
2025-07-01 15:54:19 +0900

    feat: 本番環境のホストに'fureai.onrender.com'と'www.fureai.site'を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  config.hosts << "fureai.onrender.com"
   config.hosts << 'fureai.site'
+  config.hosts << "www.fureai.site"
 end
