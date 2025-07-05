ad2859ee088d8112b6994d9be9ae8a66b5ee48d9
taka292 <66032836+taka292@users.noreply.github.com>
2025-07-05 10:39:07 +0900

    feat: Omniauthを導入し、Googleログイン機能を追加しました。また、ユーザーモデルにomniauthableを設定し、関連するGemをGemfileに追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: "users/registrations",
-    sessions: "users/sessions"
+    sessions: "users/sessions",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   # チャット機能
