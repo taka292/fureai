eccfab1af175d28a22c9c9552f642edbad66ff9f
taka292 <66032836+taka292@users.noreply.github.com>
2025-07-12 10:14:27 +0900

    fix: メッセージアバターのURL置換処理を改善し、現在のホスト名を使用するように修正

--- app/javascript/controllers/message_avatar_controller.js
+++ app/javascript/controllers/message_avatar_controller.js
@@ -8,10 +8,9 @@ export default class extends Controller {
   }
 
   fixAvatarUrl() {
-    this.avatarTargets.forEach(avatar => {
-      if (avatar.src && avatar.src.includes('example.org')) {
-        avatar.src = avatar.src.replace('http://example.org', window.location.origin)
-      }
-    })
+    if (this.hasAvatarTarget && this.avatarTarget.src && this.avatarTarget.src.includes('example.org')) {
+      // example.orgの部分だけを現在のホストに置換
+      this.avatarTarget.src = this.avatarTarget.src.replace('example.org', window.location.host)
+    }
   }
 } 
